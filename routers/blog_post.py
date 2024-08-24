9df0415e5a0bdda64aab80ad001da1d99c42136a
1666sApple <ananno.034@gmail.com>
2024-08-25 04:14:29 +0600

    Add Comments

--- routers/blog_post.py
+++ routers/blog_post.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Query
 from typing import Optional
 from pydantic import BaseModel
 
@@ -13,10 +13,26 @@ class BlogModel(BaseModel):
     nb_comments: int
     published: Optional[bool]
     
-@router.post('/new')
+@router.post('/new/{id}')
 def create_blog(blog: BlogModel, id: int, version: int):
     return {
         'id': id,
         'data': blog,
         'version': version
+    }
+    
+@router.post('/new/{id}/comment')
+def create_comment(
+    blog: BlogModel,
+    id: int,
+    comment_id: int = Query(None,
+        title='Id of the comment',
+        description = 'Description for the comment',
+        alias = 'commentId'
+    )
+):
+    return {
+        'blog': blog,
+        'title': id,
+        'comment_id': comment_id
     }
