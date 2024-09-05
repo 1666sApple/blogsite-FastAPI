3bbf583ee332b80af172f9c6f030c78fc4f7cd86
1666sApple <ananno.034@gmail.com>
2024-09-06 00:56:28 +0600

    Add articles database

--- db/models.py
+++ db/models.py
@@ -1,11 +1,24 @@
-from sqlalchemy.sql.sqltypes import Integer, String
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import Integer, String, Boolean
 from db.database import Base
 from sqlalchemy import Column
 
 class DBusers(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String)
-    email = Column(String)
-    password = Column(String)
     
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    password = Column(String, nullable=False)
+    items = relationship('DBarticles', back_populates='user')
+
+class DBarticles(Base):
+    __tablename__ = 'articles'
+    
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    published = Column(Boolean, default=False)
+    userID = Column(Integer, ForeignKey('users.id'))
+    user = relationship('DBusers', back_populates='items')
