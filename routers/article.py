46f574d5554beae90d7fe024e069207fa993cb98
1666sApple <ananno.034@gmail.com>
2024-09-09 04:33:27 +0600

    Update Articles.py

--- routers/article.py
+++ routers/article.py
@@ -2,6 +2,7 @@ from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm.session import Session
 
+from auth.oauth2 import oauth2Scheme
 from db import db_article
 from db.database import getDB
 from schemas import ArticleBase, ArticleDisplay
@@ -12,7 +13,7 @@ router = APIRouter(
 )
 
 @router.post('/', response_model=ArticleDisplay)
-def createArticle(request: ArticleBase, db: Session = Depends(getDB)):
+def createArticle(request: ArticleBase, db: Session = Depends(getDB), token: str = Depends(oauth2Scheme)):
     article = db_article.createArticle(db, request)
     if not article:
         raise HTTPException(status_code=400, detail="Article already exists")
@@ -24,21 +25,21 @@ def getAllArticles(db: Session = Depends(getDB)):
     return db_article.getAllArticle(db)
 
 @router.get('/{id}', response_model=ArticleDisplay)
-def readOneArticle(id: int, db: Session = Depends(getDB)):
+def readOneArticle(id: int, db: Session = Depends(getDB), token: str = Depends(oauth2Scheme)):
     article = db_article.getArticle(db, id)
     if not article:
         raise HTTPException(status_code=404, detail="Article not found")
     return article
     
 @router.put('/{id}/update', response_model=ArticleDisplay)
-def updateArticle(id: int, request: ArticleBase, db: Session = Depends(getDB)):
+def updateArticle(id: int, request: ArticleBase, db: Session = Depends(getDB), token: str = Depends(oauth2Scheme)):
     article = db_article.updateArticle(db, id, request)
     if not article:
         raise HTTPException(status_code=404, detail="No article found to update")
     return article
     
 @router.delete('/{id}/delete')
-def deleteArticle(id: int, db: Session = Depends(getDB)):
+def deleteArticle(id: int, db: Session = Depends(getDB), token: str = Depends(oauth2Scheme)):
     result = db_article.deleteArticle(db, id)
     if result == "No article found to delete.":
         raise HTTPException(status_code=404, detail=result)
