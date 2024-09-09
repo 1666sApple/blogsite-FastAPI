93cf21c0ae20ad315b33573a9241648fd3a3f019
1666sApple <ananno.034@gmail.com>
2024-09-09 07:18:50 +0600

    update user.py

--- routers/user.py
+++ routers/user.py
@@ -6,39 +6,36 @@ from db import db_user
 from db.database import getDB
 from schemas import UserBase, UserDisplay
 
-router = APIRouter(
-    prefix='/user',
-    tags=['user']
-)
+router = APIRouter(prefix='/user', tags=['user'])
 
 @router.post('/', response_model=UserDisplay)
-def createUser(request: UserBase, db: Session = Depends(getDB)):
-    user = db_user.createUser(db, request)
+def create_user(request: UserBase, db: Session = Depends(getDB)):
+    user = db_user.create_user(db, request)
     if not user:
         raise HTTPException(status_code=400, detail="Username or email already exists")
     return user
     
 @router.get('/', response_model=List[UserDisplay])
-def readUser(db: Session = Depends(getDB)):
-    return db_user.getAllUsers(db)
-    
+def read_users(db: Session = Depends(getDB)):
+    return db_user.get_all_users(db)
+
 @router.get('/{id}', response_model=UserDisplay)
-def readOneUser(id: int, db: Session = Depends(getDB)):
-    user = db_user.getUser(db, id)
+def read_user(id: int, db: Session = Depends(getDB)):
+    user = db_user.get_user(db, id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return user
     
 @router.put('/{id}/update', response_model=UserDisplay)
-def updateUser(id: int, request: UserBase, db: Session = Depends(getDB)):
-    user = db_user.updateUser(db, id, request)
+def update_user(id: int, request: UserBase, db: Session = Depends(getDB)):
+    user = db_user.update_user(db, id, request)
     if not user:
         raise HTTPException(status_code=404, detail="User not found or not updated")
     return user
     
 @router.delete('/{id}/delete')
-def deleteUser(id: int, db: Session = Depends(getDB)):
-    user = db_user.deleteUser(db, id)
+def delete_user(id: int, db: Session = Depends(getDB)):
+    user = db_user.delete_user(db, id)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
     return {"detail": "User deleted successfully"}
