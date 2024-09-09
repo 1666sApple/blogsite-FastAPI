105fc54777c2a36228cd2d97f8f989a2b5a94bcc
1666sApple <ananno.034@gmail.com>
2024-09-09 07:16:53 +0600

    Update oauth2.py for user authentication

--- auth/oauth2.py
+++ auth/oauth2.py
@@ -1,22 +1,48 @@
+# auth/oauth2.py
+
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from typing import Optional
 from datetime import datetime, timedelta
-from jose import jwt
+from jose import JWTError, jwt
+from sqlalchemy.orm import Session
+from db.database import getDB
+from db.models import DBusers
 
-oauth2Scheme = OAuth2PasswordBearer(tokenUrl = 'token')
+oauth2Scheme = OAuth2PasswordBearer(tokenUrl='token')
 
-SECRET_KEY = '45eef581866da63e4f39809238d8f31c376ca92f3bb4e5bbf9489a480f5f5644'
+SECRET_KEY = 'd1050c1f9e0ea5a34d37408b1fe78276835d286ca1f5001e0f1e90c558ddff28'
 ALGORITHM = 'HS256'
-ACCESS_TOKEN_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 300
 
 def createAccessToken(data: dict, expiresDelta: Optional[timedelta] = None):
     toEncode = data.copy()
     if expiresDelta:
         expire = datetime.utcnow() + expiresDelta
     else:
-        expire = datetime.utcnow() + timedelta(minutes = 15)
-    toEncode.update({
-        'exp': expire
-    })
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    toEncode.update({'exp': expire})
     encodedJwt = jwt.encode(toEncode, SECRET_KEY, algorithm=ALGORITHM)
     return encodedJwt
+
+def getCurrentUser(token: str = Depends(oauth2Scheme), db: Session = Depends(getDB)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='Could not validate credentials',
+        headers={'WWW-Authenticate': 'Bearer'}
+    )
+    
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = db.query(DBusers).filter(DBusers.username == username).first()
+    if user is None:
+        raise credentials_exception
+
+    return user
