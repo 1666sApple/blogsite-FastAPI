5bdb75152c2476783cd721c15631d7803ac8f220
1666sApple <ananno.034@gmail.com>
2024-09-09 07:20:02 +0600

    Add custom exception on different user's article retrieval, update, or delete

--- exception.py
+++ exception.py
@@ -1,3 +1,7 @@
 class StoryException(Exception):
     def __init__(self, name: str):
-        self,name = name
+        self.name = name
+
+class TermsViolationException(Exception):
+    def __init__(self, detail: str):
+        self.detail = detail
