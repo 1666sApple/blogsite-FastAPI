7293c63f7420fb078c822d4062cd70ecdbb4c31e
1666sApple <ananno.034@gmail.com>
2024-09-12 07:56:24 +0600

    add database for file upload

--- db/models.py
+++ db/models.py
@@ -2,8 +2,8 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.sql.sqltypes import Integer, String, Boolean
 from db.database import Base
-from sqlalchemy import Column
-
+from sqlalchemy import Column, DateTime, Enum
+from enum import Enum as PyEnum
 class DBusers(Base):
     __tablename__ = 'users'
     
@@ -12,6 +12,7 @@ class DBusers(Base):
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     items = relationship('DBarticles', back_populates='user')
+    files = relationship('DBFileUpload', back_populates = 'user')
 
 class DBarticles(Base):
     __tablename__ = 'articles'
@@ -22,3 +23,20 @@ class DBarticles(Base):
     published = Column(Boolean, default=False)
     userID = Column(Integer, ForeignKey('users.id'))
     user = relationship('DBusers', back_populates='items')
+
+class FileType(PyEnum):
+    IMAGE = "image"
+    DOCUMENT = "document"
+    VIDEO = "video"
+    AUDIO = "audio"
+    
+class DBFileUpload(Base):
+    __tablename__ = 'file_upload'
+
+    id = Column(Integer, primary_key=True, index=True)
+    userID = Column(Integer, ForeignKey("users.id"))
+    originalFileName = Column(String, nullable=False)
+    storedFileName = Column(String, nullable=False)
+    contentType = Column(Enum(FileType), nullable = False)
+    uploadTime = Column(DateTime, nullable=False)
+    user = relationship('DBusers', back_populates='files')
